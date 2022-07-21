e1716f78274bd3045d1e31e328eea82e57b65507
loveonex <vannamhdvt@gmail.com>
2022-07-21 12:12:04 +0700

    update

--- database/migrations/2022_07_21_050916_create_roles_table.php
+++ database/migrations/2022_07_21_050916_create_roles_table.php
@@ -13,12 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->integer('role_id');
             $table->integer('status')->default(0);
             $table->timestamps();
         });
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
